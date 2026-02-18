c2a71dde9320c91619639d9dbe12554b5049b707
Aasthax <aasthagarg2103@gmail.com>
2026-02-18 17:21:14 +0530

    Create and add error page

--- e-commerce-website/src/App.jsx
+++ e-commerce-website/src/App.jsx
@@ -2,6 +2,7 @@ import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
+import { ErrorPage } from "./pages/ErrorPage";
 import { Routes, Route } from "react-router";
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<OrdersPage />} />
         <Route path="tracking" element={<TrackingPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   );
---
+++ e-commerce-website/src/pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import "./ErrorPage.css";
+import { Link } from "react-router";
+
+export function ErrorPage() {
+  return (
+    <>
+      <div className="error-page">
+        <div className="heading">404</div>
+        <div className="sub-heading">Oops! This page was not found.</div>
+        <div className="description">
+          This page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </div>
+        <Link to="/">
+          <button className="homepage-button">GO TO HOMEPAGE</button>
+        </Link>
+      </div>
+    </>
+  );
+}
